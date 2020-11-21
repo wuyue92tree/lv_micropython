e9dcd4008c024cc4750256c8035a5c642244fb88
embeddedt <embeddedt@users.noreply.github.com>
2020-11-21 21:49:57 +0000

    Deploying to gh-pages from  @ e3588a425312f7564a2d38cb3bb681f33b0435b6 🚀

--- build_workers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ROOT=$PWD/node_modules/monaco-editor/esm/vs
-OPTS="-d bundle_out --no-source-maps --log-level 1"        # Parcel options - See: https://parceljs.org/cli.html
-parcel build $ROOT/editor/editor.worker.js $OPTS
