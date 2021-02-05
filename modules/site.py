395f7bb24f9716ecbc8c7114608909ed6afd787a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2021-02-05 00:57:00 +0000

    Deploying to gh-pages from lvgl_javascript @ 8cbadfb335462a1188968732093e6100418df059 🚀

--- modules/site.py
+++ modules/site.py
@@ -1,7 +1,7 @@
-import sys
+import usys as sys
 import builtins
 
-for name in ("sys", "__main__", "builtins"):
+for name in ("usys", "__main__", "builtins"):
     sys.modules[name] = __import__(name)
 
 import imp
